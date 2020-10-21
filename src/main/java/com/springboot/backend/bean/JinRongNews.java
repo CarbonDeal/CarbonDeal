5c1b5de83df2cb1da3b4cb98ad22628afc41b3a2
Augustine803 <1915393701@qq.com>
2020-10-21 14:35:07 +0800

    实体类

---
+++ src/main/java/com/springboot/backend/bean/JinRongNews.java
@@ -0,0 +1,51 @@
+package com.springboot.backend.bean;
+
+import java.util.Date;
+
+public class JinRongNews {
+    private String title;
+    private String href;
+    private String from;
+    private Date time;
+    private  String info;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+}
