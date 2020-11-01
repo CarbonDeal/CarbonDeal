16cafa4b4a77a02201aa9bede437d2571d203099
GuYith <48597370+GuYith@users.noreply.github.com>
2020-11-01 20:25:37 +0800

    邮件的验证，参考教程https://blog.csdn.net/IndexMan/article/details/89409512按照我们的实现进行了一些修改，补充在了regist,添加了方法getUserByActiveCode/modifyUser等， 对于User添加了属性email,active_code,active_status；补充了部分的注释

---
+++ src/main/java/com/springboot/backend/bean/IDUtils.java
@@ -0,0 +1,21 @@
+package com.springboot.backend.bean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+public class IDUtils {
+    public static final Logger logger = LoggerFactory.getLogger(IDUtils.class);
+
+    public static String getUUID(){
+        return UUID.randomUUID().toString().replace("-","");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getUUID());
+        logger.debug("test");
+    }
+
+}
